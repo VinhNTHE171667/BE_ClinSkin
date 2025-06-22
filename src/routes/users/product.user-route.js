09c090be0e8cdb52940c249a6881ee6333791f8d
VinhNTHE171667 <136615934+VinhNTHE171667@users.noreply.github.com>
2025-06-22 22:37:14 +0700

    SearchProductPage

--- src/routes/users/product.user-route.js
+++ src/routes/users/product.user-route.js
@@ -3,10 +3,13 @@ import {
   getProductDetailBySlug,
   getProductHome,
 } from "../../controllers/product-base.controller.js";
+import { getProductById, getProducts } from "../../controllers/productController.js";
 
 const router = express.Router();
 
 router.get("/home", getProductHome);
 router.get("/detail/:slug", getProductDetailBySlug);
+router.get("/", getProducts);
+router.get("/:id", getProductById)
 
 export default router;
