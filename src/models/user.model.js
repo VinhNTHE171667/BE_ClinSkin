f72435929248fe6da532e7fa99a7b45f46c59101
VinhNTHE171667 <136615934+VinhNTHE171667@users.noreply.github.com>
2025-06-28 15:30:23 +0700

    Update model user and updateUser

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,6 +1,13 @@
 import mongoose from "mongoose";
 import bcrypt from "bcryptjs";
 
+const addressSchema = new mongoose.Schema({
+    province: { type: String, required: true },
+    district: { type: String, required: true },
+    ward: { type: String, required: true },
+    detail: { type: String}
+}, { _id: false });
+
 export const UserSchema = new mongoose.Schema(
     {
         googleId: {
@@ -28,6 +35,15 @@ export const UserSchema = new mongoose.Schema(
                 type: String,
                 default: "",
             },
+        },
+        phone: {
+            type: String,
+            unique: true,
+            match: [/^[0-9]{10}$/, "Số điện thoại phải gồm đúng 10 chữ số (0–9), không chứa chữ hoặc ký tự đặc biệt."],
+        },
+        address: {
+            type: addressSchema,
+            required: true
         },
         isActive: {
             type: Boolean,
