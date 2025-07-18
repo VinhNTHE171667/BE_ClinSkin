c89cb14615a6e47c74cbf71a0cd79a5d0488b9d6
Huy Nguyen <huy861149@gmail.com>
2025-07-18 13:16:58 +0700

    fixroutereview

--- src/routes/review/review-route.js
+++ src/routes/review/review-route.js
@@ -1,9 +1,9 @@
 import express from "express";
 import { getReview, updateReplyByAdmin,deleteReviewById,toggleDisplayReview} from "../../controllers/review.controller.js";
 const router = express.Router();
-router.get("/getReview", getReview);v  // lấy danh sách review
-router.put("/:id/reply", updateReplyByAdmin); // cập nhật phản hồi
-router.delete("/:id", deleteReviewById); // xóa review
-router.patch("/:id/toggle-display", toggleDisplayReview); // đổi trạng thái hiển thị
+router.get("/getReview", getReview); 
+router.put("/:id/reply", updateReplyByAdmin); 
+router.delete("/:id", deleteReviewById); 
+router.patch("/:id/toggle-display", toggleDisplayReview); 
 export default router;
 
