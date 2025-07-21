9f4a0ec8efa1970ab7547f5c5bb8ad3a2d1a87f4
anhnnhe176642 <anhnnhe176642@fpt.edu.vn>
2025-07-21 17:13:51 +0700

    Refactor address controller and routes to associate addresses with users and enforce authorization

--- src/routes/users/address.user-route.js
+++ src/routes/users/address.user-route.js
@@ -1,13 +1,13 @@
 import express from "express";
-
+import { authMiddlewareUser } from "../../middleware/auth.middleware.js";
 import { createAddress, deleteAddress, getAllAddress, setDefaultAddress, updateAddress } from "../../controllers/address.controller.js";
 
 const router = express.Router();
 
-router.get("/", getAllAddress);
-router.post("/", createAddress);
-router.put("/:id", updateAddress);
-router.delete("/:id", deleteAddress);
-router.patch('/:id/set-default', setDefaultAddress);
+router.get("/", authMiddlewareUser, getAllAddress);
+router.post("/", authMiddlewareUser, createAddress);
+router.put("/:id", authMiddlewareUser, updateAddress);
+router.delete("/:id", authMiddlewareUser, deleteAddress);
+router.patch('/:id/set-default', authMiddlewareUser, setDefaultAddress);
 
 export default router;
